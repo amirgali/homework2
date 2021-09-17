6604e2af240d64acfcd3b62d197036e39d5efcbb
Amirgali <aturalinov@yandex.ru>
2021-09-17 19:34:29 +0300

    add cornerRadius for singUpButton. add check

--- homeWork2/ViewController.swift
+++ homeWork2/ViewController.swift
@@ -8,15 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var surnameTF: UITextField!
+    @IBOutlet weak var birthdayTF: UITextField!
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var singUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        singUpButton.layer.cornerRadius = 12
     }
-
+   
+    @IBAction func singUp(_ sender: Any) {
+        guard let name = nameTF.text, let surname = surnameTF.text, let birthday = birthdayTF.text, let login = loginTF.text, let password = passwordTF.text else { return }
+        let condition: Bool = !name.isEmpty && !surname.isEmpty && !birthday.isEmpty && !login.isEmpty && !password.isEmpty
+        singUpButton.backgroundColor = condition ? .green : .red
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
 }
 
