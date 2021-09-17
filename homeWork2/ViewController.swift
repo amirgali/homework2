4882c4c3d17519bc95c79145f07cafd56119b6db
Amirgali <aturalinov@yandex.ru>
2021-09-17 15:07:54 +0300

    Creat project

---
+++ homeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork2
+//
+//  Created by Амиргали Туралинов on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
