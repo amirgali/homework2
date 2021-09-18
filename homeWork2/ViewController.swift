fca0373570d8c4be9c3aa4b144998c7cdcd6eb11
Amirgali <aturalinov@yandex.ru>
2021-09-18 18:38:29 +0300

    add cornerRadius for textFields

--- homeWork2/ViewController.swift
+++ homeWork2/ViewController.swift
@@ -16,14 +16,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var singUpButton: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
+        nameTF.layer.borderColor = UIColor(red: 0.72, green: 0.72, blue: 0.74, alpha: 1).cgColor
+        surnameTF.layer.borderColor = UIColor(red: 0.72, green: 0.72, blue: 0.74, alpha: 1).cgColor
+        birthdayTF.layer.borderColor = UIColor(red: 0.72, green: 0.72, blue: 0.74, alpha: 1).cgColor
+        loginTF.layer.borderColor = UIColor(red: 0.72, green: 0.72, blue: 0.74, alpha: 1).cgColor
+        passwordTF.layer.borderColor = UIColor(red: 0.72, green: 0.72, blue: 0.74, alpha: 1).cgColor
+        
+        nameTF.layer.cornerRadius = 12
+        surnameTF.layer.cornerRadius = 12
+        birthdayTF.layer.cornerRadius = 12
+        loginTF.layer.cornerRadius = 12
+        passwordTF.layer.cornerRadius = 12
         singUpButton.layer.cornerRadius = 12
     }
    
     @IBAction func singUp(_ sender: Any) {
+        
         guard let name = nameTF.text, let surname = surnameTF.text, let birthday = birthdayTF.text, let login = loginTF.text, let password = passwordTF.text else { return }
         let condition: Bool = !name.isEmpty && !surname.isEmpty && !birthday.isEmpty && !login.isEmpty && !password.isEmpty
         singUpButton.backgroundColor = condition ? .green : .red
@@ -31,6 +42,12 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    func setView(_ view: ViewType, space: CGFloat) -> UIView {
+        let spaceView = UIView(frame: CGRect(x: 0, y: 0, width: space, height: 1))
+        setView(view, with: spaceView)
+        return spaceView
     }
     
 }
