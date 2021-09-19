eedf7e87445c18b249f020f768f73c6e161b5f54
Amirgali <aturalinov@yandex.ru>
2021-09-19 19:02:23 +0300

    add check textField Birthday and add alert

--- homeWork2/ViewController.swift
+++ homeWork2/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -39,22 +40,48 @@ class ViewController: UIViewController {
         birthdayTF.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0)
         loginTF.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0)
         passwordTF.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0)
-        
-        birthdayTF.keyboardType = .decimalPad
     }
-   
+    
     @IBAction func singUp(_ sender: Any) {
-        
         guard let name = nameTF.text, let surname = surnameTF.text, let birthday = birthdayTF.text, let login = loginTF.text, let password = passwordTF.text else { return }
+        
+        let dateString = birthday
+        print("\(dateString)")
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        
+        //        if let _: NSDate = (dateFormatter.date(from: dateString)! as NSDate?) {
+        //            print("date is valid")
+        //        } else {
+        //            print("date is invalid")
+        //
+        //        }
+        guard let _ = dateFormatter.date(from: dateString) else {
+            let alert = UIAlertController(title: "Wrong format", message: "Please enter your birthday in format dd.mm.yyyy", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            singUpButton.backgroundColor = .red
+            birthdayTF.backgroundColor = .red
+            
+            return present(alert, animated: true, completion: nil)
+        }
+        
+        //        guard let _ = Double(birthdayTF.text!) else {
+        //            let alert = UIAlertController(title: "Wrong format", message: "Please enter your birthday", preferredStyle: .alert)
+        //            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        //            alert.addAction(okAction)
+        //            return present(alert, animated: true, completion: nil)
+        //        }
         
         let condition: Bool = !name.isEmpty && !surname.isEmpty && !birthday.isEmpty && !login.isEmpty && !password.isEmpty
         singUpButton.backgroundColor = condition ? .green : .red
+        birthdayTF.backgroundColor = .white
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-   
+    
     
     
 }
